360b86f39f0855529d2f57b89c1c653853920467
Dorai Sitaram <ds26gte@gmail.com>
2025-01-30 09:01:23 -0500

    use aux *.js files in src; use subcomponents

--- src/App.js
+++ src/App.js
@@ -1,32 +1,5 @@
-import { useState } from 'react';
+import Gallery from './Gallery';
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue((value === 'X') ? null : 'X');
-  }
-  return <button className="square" onClick={handleClick}>{value}</button>;
-}
-
-export default function Board() {
-  return (
-    <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>    
-    </>
-  );
+export default function App() {
+  return <Gallery />;
 }
